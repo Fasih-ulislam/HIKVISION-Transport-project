2986ae7f0375b030eaa1d2abf605f4cda9cdb69d
Muhammad Fasih <muhammadfasih969@gmail.com>
2026-06-06 12:42:54 +0500

    updated the log route

--- routes/loggingRoutes.js
+++ routes/loggingRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const loggingController = require("../controllers/loggingController");
 
-router.get("/", loggingController.deviceLogs);
+router.get("/device", loggingController.deviceLogs);
 
 module.exports = router;
